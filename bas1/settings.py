d6363a659c6d4fe11277b1a5c51661709a263816
srm1071 <36409839+srm1071@users.noreply.github.com>
2021-10-17 22:39:56 +0530

    Update settings.py

--- bas1/settings.py
+++ bas1/settings.py
@@ -76,7 +76,12 @@ WSGI_APPLICATION = 'bas1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 import dj_database_url
 db_from_env=dj_database_url.config()
 DATABASES['default'].update(db_from_env)
