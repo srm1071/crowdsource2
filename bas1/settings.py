e20109a4724b6487f178672a733f7e36782323ec
srm1071 <36409839+srm1071@users.noreply.github.com>
2021-10-17 21:19:51 +0530

    Update settings.py

--- bas1/settings.py
+++ bas1/settings.py
@@ -79,8 +79,7 @@ WSGI_APPLICATION = 'bas1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 
